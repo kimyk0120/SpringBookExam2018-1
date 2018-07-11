6ea2c93bdea6cc70da3fc566a0267fffaa5b158c
YoungKim <kimyk0120@hotmail.com>
2018-07-11 17:44:51 +0900

    Change-Id: I0473b23f27b3da91cd0a504a91f10c3e6515f62c Signed-off-by: YoungKim <kimyk0120@hotmail.com>

---
+++ SpringBookTest2018-1/src/main/java/yk/book/springExam2018/service/WelcomeService.java
@@ -0,0 +1,10 @@
+package yk.book.springExam2018.service;
+
+public interface WelcomeService {
+
+	public String getHelloMsg() throws Exception;
+
+
+
+	
+}
---
+++ SpringBookTest2018-1/src/main/java/yk/book/springExam2018/service/WelcomeServiceImpl.java
@@ -0,0 +1,25 @@
+package yk.book.springExam2018.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WelcomeServiceImpl implements WelcomeService {
+
+	@Override
+	public String getHelloMsg() throws Exception {
+		return "hello";
+	}
+	
+//	@Autowired
+//	private SqlSession sqlSession;
+
+
+
+	
+	
+}//.class
+
+
+
+
+//endDoc
