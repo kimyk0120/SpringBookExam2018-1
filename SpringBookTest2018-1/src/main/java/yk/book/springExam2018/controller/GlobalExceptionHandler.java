e339ee6c3a736ba4a6ff306f2d09353b4c7cd94b
YoungKim <kimyk0120@hotmail.com>
2018-07-31 13:57:24 +0900

    Signed-off-by: YoungKim <kimyk0120@hotmail.com>

--- SpringBookTest2018-1/src/main/java/yk/book/springExam2018/controller/GlobalExceptionHandler.java
+++ SpringBookTest2018-1/src/main/java/yk/book/springExam2018/controller/GlobalExceptionHandler.java
@@ -1,19 +1,15 @@
 package yk.book.springExam2018.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice 
 public class GlobalExceptionHandler {
 
 		
 		@ExceptionHandler
-		@ResponseStatus(HttpStatus.NOT_FOUND)
 		public String handleException(Exception e,Model model){
-			
 			model.addAttribute("errMsg", e.getMessage());
 			return "/error/systemError";
 		}
