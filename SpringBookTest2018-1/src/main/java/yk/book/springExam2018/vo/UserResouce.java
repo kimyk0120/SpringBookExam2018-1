632e8a84c3a3dfa7c96cef84b4a7e659ac33e781
YoungKim <kimyk0120@hotmail.com>
2018-07-31 15:43:34 +0900

    Signed-off-by: YoungKim <kimyk0120@hotmail.com>

---
+++ SpringBookTest2018-1/src/main/java/yk/book/springExam2018/vo/UserResouce.java
@@ -0,0 +1,18 @@
+package yk.book.springExam2018.vo;
+
+import java.io.Serializable;
+
+public class UserResouce implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String login;
+	private Integer id;
+	private String name;
+	private String blog;
+	
+	
+	
+	
+	
+}//.class
