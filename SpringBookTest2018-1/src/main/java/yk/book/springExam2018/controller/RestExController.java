4527447f746bab75b3811d53953ef7ac8c3dd32c
kimyk0120 <kimyk0120@hotmail.com>
2018-08-02 18:57:05 +0900

    Signed-off-by: kimyk0120 <kimyk0120@hotmail.com>

--- SpringBookTest2018-1/src/main/java/yk/book/springExam2018/controller/RestExController.java
+++ SpringBookTest2018-1/src/main/java/yk/book/springExam2018/controller/RestExController.java
@@ -1,10 +1,12 @@
 package yk.book.springExam2018.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import yk.book.springExam2018.service.BookService;
@@ -18,12 +20,21 @@ public class RestExController {
 	BookService bookService;
 	
 	@RequestMapping(value = "{bookId}", method = RequestMethod.GET)
-	@ResponseBody
 	public BookResouce getBook(@PathVariable String bookId) throws Exception{
 		System.out.println("test ," +bookId);
 		BookResouce resouce = bookService.find(bookId);
 		
 		return resouce;
+	} 
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> createBook(
+			@Validated @RequestBody BookResouce newResouce) throws Exception{
+		System.out.println("createBook");
+		
+		BookResouce cResource = new BookResouce();
+		
+		return null;
 	} 
 	
 	
